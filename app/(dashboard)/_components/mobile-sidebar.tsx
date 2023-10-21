3eccf8433fdd459821b23b93509e8fc56a88138b
leobewater <mmbee888@gmail.com>
2023-10-21 08:37:05 -0400

    add mobile sidebar and navbar route

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,7 +1,16 @@
-import { Menu } from 'lucide-react'
+import { Sidebar } from "@/app/(dashboard)/_components/sidebar";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 export const MobileSidebar = () => {
   return (
-    <Menu />
-  )
-}
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
---
+++ components/navbar-routes.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NavbarRoutes = () => {
+  return (
+    <div>NavbarRoutes</div>
+  )
+}
