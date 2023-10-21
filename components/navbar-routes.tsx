dfe649e40868c7edc95e4ad68d7fb7fdcb4a66c8
leobewater <mmbee888@gmail.com>
2023-10-21 11:54:14 -0400

    update exit button style

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -16,7 +16,7 @@ export const NavbarRoutes = () => {
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
         <Link href="/">
-          <Button>
+          <Button size="sm" variant="ghost">
             <LogOut className="h-4 w-4 mr-2" />
             Exit
           </Button>
