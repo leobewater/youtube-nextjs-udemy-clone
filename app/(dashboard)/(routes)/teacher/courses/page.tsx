ade780218a8234bdd2b2dea2ff970f66ffb15e8c
leobewater <mmbee888@gmail.com>
2023-10-21 12:19:30 -0400

    set up course create form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+  return <div className="p-6">
+    <Link href="/teacher/create">
+      <Button>New Course</Button>
+    </Link>
+  </div>;
 };
 
 export default CoursesPage;
