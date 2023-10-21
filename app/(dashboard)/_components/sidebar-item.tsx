b860c85c3673ee1e3616e9ec8533593bcc033006
leobewater <mmbee888@gmail.com>
2023-10-21 08:20:10 -0400

    add sidebar route items

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+export const SidebarItem = ({ icon, label, href }: SidebarItemProps) => {
+  return <div>{label}</div>;
+};
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { SidebarItem } from "@/app/(dashboard)/_components/sidebar-item";
+import { Compass, Layout } from "lucide-react";
+
+const guestRoutes = [
+  { icon: Layout, label: "Dashboard", href: "/" },
+  { icon: Compass, label: "Browse", href: "/search" },
+];
+
+export const SidebarRoutes = () => {
+  const routes = guestRoutes;
+
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
