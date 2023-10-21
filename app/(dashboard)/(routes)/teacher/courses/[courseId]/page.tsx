d09f63689f36714472c074ee30b2dc036278e59c
leobewater <mmbee888@gmail.com>
2023-10-21 14:03:35 -0400

    add course detail page

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react'
+
+const CourseIdPage = ({ params }:{params:{courseId: string}}) => {
+
+  return (
+    <div>CourseIdPage {params.courseId}</div>
+  )
+}
+
+export default CourseIdPage
