bd0481e0402e183a1d9138f889b12de02a8350cb
leobewater <mmbee888@gmail.com>
2023-10-21 11:56:12 -0400

    swap sidebar routes based on guest or teacher mode

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { SidebarItem } from "@/app/(dashboard)/_components/sidebar-item";
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   { icon: Layout, label: "Dashboard", href: "/" },
   { icon: Compass, label: "Browse", href: "/search" },
 ];
 
+const teacherRoutes = [
+  { icon: List, label: "Courses", href: "/teacher/courses" },
+  { icon: BarChart, label: "Analytics", href: "/teacher/analytics" },
+];
+
 export const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
   return (
     <div className="flex flex-col w-full">
