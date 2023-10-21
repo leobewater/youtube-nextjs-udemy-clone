26d27d51339fa37c7373094c4807d3467eaa3f5c
leobewater <mmbee888@gmail.com>
2023-10-21 07:46:38 -0400

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Team Chat Application",
-  description: "Chat app",
+  title: "LMS Platform",
+  description: "Learning Online Courses App",
 };
 
 export default function RootLayout({
