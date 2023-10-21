c5c2f81e55feafe9a89255dc1d3047ec652408f7
leobewater <mmbee888@gmail.com>
2023-10-21 08:14:21 -0400

    add sidebar and logo

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,11 @@
+import { Logo } from "@/app/(dashboard)/_components/logo";
+
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+    </div>
+  );
+};
