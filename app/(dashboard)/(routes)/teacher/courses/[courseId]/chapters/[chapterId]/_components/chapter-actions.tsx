7f3a34c6a3ac949c439febbd20187dc1b56bc6b8
leobewater <mmbee888@gmail.com>
2023-10-25 14:57:38 -0400

    add banner to chapter page and chapter actions component

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ChapterActionsProps {
+  disabled: boolean;
+  courseId: string;
+  chapterId: string;
+  isPublished: boolean;
+}
+
+export const ChapterActions = ({
+  disabled,
+  courseId,
+  chapterId,
+  isPublished,
+}: ChapterActionsProps) => {
+  return <div>ChapterActions</div>;
+};
