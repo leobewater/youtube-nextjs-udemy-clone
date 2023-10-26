10da94470c9b19aa8c723a254139f9da10d32aff
leobewater <mmbee888@gmail.com>
2023-10-26 10:07:00 -0400

    play video

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -2,6 +2,9 @@
 
 import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
+import { Loader2, Lock } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface VideoPlayerProps {
   playbackId: string;
@@ -22,6 +25,31 @@ export const VideoPlayer = ({
   completeOnEnd,
   title,
 }: VideoPlayerProps) => {
-  
-  return <div>VideoPlayer</div>;
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">This chapter is locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
 };
